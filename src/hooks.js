7124a52bad8b6afcaf6c4154b149f5b6a9fbfa5c
Jasper Dunn <jasperdunn93@gmail.com>
2019-04-25 12:39:31 +1000

    add functions for setting individual input values and errors

--- src/hooks.js
+++ src/hooks.js
@@ -15,7 +15,8 @@ import {
  * - formValues - Object where each key is the input id and the value is the current input value.
  * - resetFormValues - Function that resets the form values to the initial state.
  * - resetInputValue - Function that resets the input value to it's initial state.
- * - updateInputValue - Function typically passed to onChange that updates the input value in the form state.
+ * - updateInputValue - Function called via an event handler that updates an input value.
+ * - setInputValue - Function that updates an input value.
  */
 export function useFormValues(initialFormValues) {
   const [formValues, setFormValues] = useState(initialFormValues)
@@ -24,7 +25,9 @@ export function useFormValues(initialFormValues) {
     formValues,
     resetFormValues: () => setFormValues(initialFormValues),
     resetInputValue: inputName => resetInputValue(inputName, setFormValues),
-    updateInputValue: event => updateInputValue(event, setFormValues)
+    updateInputValue: event => updateInputValue(event, setFormValues),
+    setInputValue: (name, value) =>
+      setFormValues(prevFormValues => ({ ...prevFormValues, [name]: value }))
   }
 }
 
@@ -39,7 +42,8 @@ export function useFormValues(initialFormValues) {
  * - validateInputValue - Function that runs validation on a single input.
  * - clearFormErrors - Function that clears all the form errors.
  * - clearInputErrors - Function that clears the inputs errors.
- * - setFormErrors - Function that sets the formErrors.
+ * - setFormErrors - Function that sets the form errors.
+ * - setInputErrors - Function that sets an inputs errors.
  */
 export function useFormErrors(formValidations = {}) {
   const emptyObj = {}
@@ -55,6 +59,8 @@ export function useFormErrors(formValidations = {}) {
       setInputValidationErrors(event, setFormErrors, formValidations),
     clearFormErrors: () => setFormErrors(emptyObj),
     clearInputErrors: inputName => clearInputErrors(inputName, setFormErrors),
-    setFormErrors
+    setFormErrors,
+    setInputErrors: (name, value) =>
+      setFormErrors(prevFormErrors => ({ ...prevFormErrors, [name]: value }))
   }
 }
