0796ce4b3463a759075581fd1d0431b0d2684ac0
Jasper Dunn <jasperdunn93@gmail.com>
2019-07-13 11:09:05 +1000

    Updates

---
+++ precache-manifest.d0bffef81e9437782f0667ac5bb19083.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "362424aa6b843967ca31e85632f92cbd",
+    "url": "/react-form-hooks/index.html"
+  },
+  {
+    "revision": "7e584974d17ee8dcd31c",
+    "url": "/react-form-hooks/static/js/2.cc16dd21.chunk.js"
+  },
+  {
+    "revision": "922f6ec44fc3863f3978",
+    "url": "/react-form-hooks/static/js/main.972f57ab.chunk.js"
+  },
+  {
+    "revision": "4aa2353246debbf7c460",
+    "url": "/react-form-hooks/static/js/runtime~main.6933531f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-form-hooks/precache-manifest.d63cc9c7f955d7b3ca3143882297a1d7.js"
+  "/react-form-hooks/precache-manifest.d0bffef81e9437782f0667ac5bb19083.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-form-hooks/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-form-hooks/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
